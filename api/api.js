24e0a233bd8d5ae6aab377456c5878296205c027
psenger <philip.a.senger@cngrgroup.com>
2015-01-01 22:29:25 +1100

    Added basic jet

--- api/api.js
+++ api/api.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var User = require('./models/User.js');
+var jwt = require('./services/jwt.js');
 var app = express();
 app.use( bodyParser.json() );
 app.use( function(req,res,next){
@@ -9,18 +11,15 @@ app.use( function(req,res,next){
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
-var User = mongoose.model('User',{
-    email: String,
-    password: String
-});
 app.post('/register',function(req,res){
     var user = req.body;
-    var newUser = new User({
+    var newUser = new User.model({
         email:user.email,
         password:user.password
     });
     newUser.save(function(err){
-        res.status(200).json(newUser);
+        //res.status(200).json(newUser);
+        res.status(200).send(newUser.toJSON());
     }); 
 });
 mongoose.connect("mongodb://192.168.33.10:27017/psjwt");
