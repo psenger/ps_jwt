926a9fcdd43427fdfc389f59a5eba10de7493045
psenger <philip.a.senger@cngrgroup.com>
2015-01-04 08:46:41 +1100

    Updated

--- frontend/app/scripts/app.config.js
+++ frontend/app/scripts/app.config.js
@@ -1,16 +1,18 @@
 'use strict';
- 
-angular.module('psJwtApp').config(function($urlRouterProvider, $stateProvider){
-    $urlRouterProvider.otherwise('/');
-
-    $stateProvider
-        .state('main',{
-            url:'/',
-            templateUrl:'/views/main.html'
-        })
-        .state('register',{
-            url:'/register',
-            templateUrl:'/views/register.html',
-            controller: 'RegisterCtrl'
-        });
+angular.module('psJwtApp').config(function($urlRouterProvider, $stateProvider) {
+  $urlRouterProvider.otherwise('/');
+  $stateProvider
+    .state('main', {
+      url: '/',
+      templateUrl: '/views/main.html'
+    })
+    .state('register', {
+      url: '/register',
+      templateUrl: '/views/register.html',
+      controller: 'RegisterCtrl'
+    })
+    .state('logout', {
+      url: '/logout', 
+      controller: 'LogoutCtrl'
+    });
 });
--- frontend/app/scripts/services/authtoken.js
+++ frontend/app/scripts/services/authtoken.js
@@ -1,21 +1,27 @@
 'use strict';
 angular.module('psJwtApp').factory('authToken', function($window) {
-    var storage = $window.localStorage;
-    var cachedToken;
-    return {
-      setToken: function(token) {
-        cachedToken = token;
-        storage.setItem('userToken', token);
-      },
-      getToken: function() {
-        if (!cachedToken){
-            cachedToken = storage.getItem('userToken');
-        }
-        return cachedToken;
-      },
-      isAuthenticated: function() {
-        // take result of getToken, cast to boolean, inverse it
-        return !!this.getToken();
+  var storage = $window.localStorage;
+  var cachedToken;
+  var userToken = 'userToken';
+  var authToken = {
+    setToken: function(token) {
+      cachedToken = token;
+      storage.setItem(userToken, token);
+    },
+    getToken: function() {
+      if (!cachedToken) {
+        cachedToken = storage.getItem(userToken);
       }
-    };
-  });
+      return cachedToken;
+    },
+    isAuthenticated: function() {
+      // take result of getToken, cast to boolean, inverse it
+      return !!authToken.getToken();
+    },
+    removeToken: function() {
+      cachedToken = null;
+      storage.removeItem(userToken);
+    }
+  };
+  return authToken;
+});
